2eaf55a2a745b5ad1f168944482240e121aee35a
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 15:31:32 +0545

    encrypt

---
+++ encrypt.java
@@ -0,0 +1,17 @@
+
+public class encrypt {
+    public static void main(String[] args) {
+        char grade='b';
+        //encrypt
+        grade=(char)(grade+8);
+        System.out.println(grade);
+        //decrypt
+        grade=(char) (grade-8);
+        System.out.println(grade);
+
+        
+        
+    }
+
+    
+}
