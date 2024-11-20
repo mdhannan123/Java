e85ea223573c7704fee70fb9068d9cd54777984b
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 16:18:42 +0545

    string

--- encrypt.java
+++ encrypt.java
@@ -1,16 +1,7 @@
 import java.util.Scanner;
 public class encrypt {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter any number");
-        int a= sc.nextInt();
-          if(a>8){
-            System.out.println("the number is greater than 8");
         
-          }
-          else{
-            System.out.println("the number is not greate than 8");
-          }
           
         // char grade='b';
         // //encrypt
@@ -19,13 +10,9 @@ public class encrypt {
         // //decrypt
         // grade=(char) (grade-8);
         // System.out.println(grade);
-        // int v= 3;
-        // int u= 2;
-        // int a=1;
-        // int s= 2;
-        // float ep= (float)(v*v-u*u)/2*a*s;
-        // System.out.println(ep);
-
+        int x=7;
+        int a= 7*49/7+35/7;
+        System.out.println(a);
         
         
     }
---
+++ string.java
@@ -0,0 +1,18 @@
+public class string {
+    public static void main(String[] args) {
+        String name= "Hannan";
+        // int value = name.length();// calculates the length of the string
+        // System.out.println(value);
+        // String value1= name.toLowerCase();// converts to lowercase
+        // System.out.println(value1);
+        // String value2 = name.toUpperCase();//conberts to uppercase
+        // System.out.println(value2);
+        // System.out.println(name.substring(2));//starts from the mentioned index
+        // System.out.println(name.substring(2,5));//starts from the mentioned index but doesnt include the last index
+        // System.out.println(name.replace('a','s'));//replaces the letter
+        // System.out.println(name.startsWith("Han"));//starts from the mentioned index
+        System.out.println(name.endsWith("an"));//starts from the mentioned index
+        
+    }
+    
+}
