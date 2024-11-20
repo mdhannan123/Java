de0a45bcfc0163b80a465790e8a57b06c1fd32e8
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 15:42:48 +0545

    greater

--- greater.java
+++ greater.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class encrypt {
+public class greater {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter any number");
@@ -12,20 +12,7 @@ public class encrypt {
             System.out.println("the number is not greate than 8");
           }
           
-        // char grade='b';
-        // //encrypt
-        // grade=(char)(grade+8);
-        // System.out.println(grade);
-        // //decrypt
-        // grade=(char) (grade-8);
-        // System.out.println(grade);
-        // int v= 3;
-        // int u= 2;
-        // int a=1;
-        // int s= 2;
-        // float ep= (float)(v*v-u*u)/2*a*s;
-        // System.out.println(ep);
-
+    
         
         
     }
