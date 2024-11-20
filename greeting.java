aa01270fab5ec23a775f2249563aed3d4f9c0319
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 12:57:22 +0545

    greeting

---
+++ greeting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class greeting {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter ur name");
+        String a= sc.next();
+        System.out.println("Hello " + a + ", have a good day!");
+
+        
+    }
+    
+}
