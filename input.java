b2e00adc4ece3f57d3cdfbcc39a75510f62dfe8a
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 12:18:13 +0545

    input

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter 1st number");
+        int a = sc.nextInt();
+        System.out.println("enter 2nd number");
+        int b= sc.nextInt();
+        int c= a+b;
+        System.out.println(" the sum of these two number is "+c);
+
+        
+    }
+
+    
+}
