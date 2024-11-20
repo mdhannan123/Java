d428363c9a58322572de6e04cc50de0b0666d43b
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 17:23:31 +0545

    swi

--- swi.java
+++ swi.java
@@ -3,19 +3,30 @@ public class swi{
     public static void main(String[] args){
        
         Scanner sc= new Scanner(System.in);
-        System.out.println("enter ur age");
-        int age=sc.nextInt();
-        switch(age){
-            case 18:
-                System.out.println("u r eligible to vote");
+        System.out.println("enter number from 1-7");
+        int days=sc.nextInt();
+        switch(days){
+            case 1:
+                System.out.println("Monday");
                 break;
             
-            case 50:
-                System.out.println("its time to retire");
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+                case 3:
+                System.out.println("Wednesday");
                 break;
             
+            case 5:
+                System.out.println("Thursday");
+                break;
+
+                case 6:
+                System.out.println("Friday");
+                break;
             default:
-                System.out.println("you had a great life");
+                System.out.println("Sunday");
             
         }
 
