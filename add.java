0bf3d08ad0a4d9ac69d10bd464010d82160d2cb4
mdhannan123 <mdhanaan50@gmail.com>
2024-11-20 11:49:32 +0545

    add

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add {
+        public static void main(String[] args){
+            int a=5;
+            int b=10;
+            int c=2;
+            int sum=a+b+c;
+            System.out.println(sum);
+
+        }
+    }
+    
