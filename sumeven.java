1f41e852c3d068102527a8041710902bf7be64a0
mdhannan123 <mdhanaan50@gmail.com>
2024-11-25 15:36:30 +0545

    sum

---
+++ sumeven.java
@@ -0,0 +1,31 @@
+public class sumeven {
+    public static void main(String[] args) {
+        // int n=3;
+        // int sum=0;
+        // for(int i=0; i<n; i++)
+        //     sum=sum+(2*i);
+        //     System.out.print("sum of even number "+ sum);
+        // int n=10;
+        // for(int i=10; i>=1;i--){
+        //     System.out.printf("%d * %d = %d \n", n,i,n*i);// reverse order multiplication
+        // }
+        // int n=10;
+        // for(int i=1; i<n;i++){
+        //     System.out.printf("%d * %d = %d \n", n,i,n*i);
+        // }
+        int n=5;
+        int i=1;
+        int factorial=1;
+        while(i<=n){
+            factorial *= i;
+            i++;
+        }
+        System.out.println(factorial);
+            
+        }
+        }
+
+        
+        
+    
+
