d3683052eb4419503cf6f72c51c357c38810a405
mdhannan123 <mdhanaan50@gmail.com>
2024-11-25 14:21:38 +0545

    natural

---
+++ naturalnum.java
@@ -0,0 +1,11 @@
+public class naturalnum {
+    public static void main(String[] args) {
+        int i=100;
+        while ( i<=200){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+    
+}
