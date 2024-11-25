e9bcd047f75038b26c93c1cf661cf91fedc07d98
mdhannan123 <mdhanaan50@gmail.com>
2024-11-25 14:35:23 +0545

    naturalnum

--- naturalnum.java
+++ naturalnum.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 public class naturalnum {
     public static void main(String[] args) {
-        int i=100;
-        while ( i<=200){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter any number to get natural number to that number");
+        int user= sc.nextInt();
+        int i=1;
+        do {
             System.out.println(i);
             i++;
+        }while(i<=user);
+
         }
+        
+        // while ( i<=200){
+        //     System.out.println(i);
+        //     i++;
+        // }
         
     }
     
-}
